798a5e32285b4e1a1ba3124c9ba413e8935db7a8
Arlin <arlin.grijalba@gmail.com>
2017-03-09 12:39:04 -0800

    Added bootstrap, add/delete/orderBy todos functionality

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,14 +1,100 @@
 angular
-  .module('app')
-  .controller('HomeController', HomeController);
+    .module('app')
+    .controller('HomeController', HomeController);
 
-function HomeController(){
-  var vm = this;
+function HomeController() {
+    var vm = this;
 
-  vm.todos = [];
+    // variables
+    vm.orderByTodoStartingByA = false;
+    vm.orderByPriorityStartingByA = false;
+    vm.todos = [{
+            "todo": 'Be happy',
+            "priority": 'High',
+            "done": true
+        },
+        {
+            "todo": 'Example 2',
+            "priority": 'Low',
+            "done": false
+        },
+        {
+            "todo": 'Finish reading YDKJS',
+            "priority": 'High',
+            "done": false
+        },
+        {
+            "todo": 'Eat',
+            "priority": 'Low',
+            "done": true
+        }
+    ];
+    vm.priorities = [
+        'Priority',
+        'High',
+        'Medium',
+        'Low'
+    ];
 
-  vm.click = function click(){
-    vm.todos.push(vm.newTodo);
-    vm.newTodo = ' ';
-  }
+    vm.selectedPriority = vm.priorities[0];
+
+    // when user clicks submit button, add a new todo
+    vm.click = function click() {
+        vm.newObject = createTodo(vm.todo, vm.selectedPriority);
+        vm.todos.push(vm.newObject);
+        vm.todo = '';
+    }
+
+    function createTodo(todo, priority) {
+        return {
+            "todo": todo,
+            "priority": priority
+        };
+    }
+
+    // order todo list by todo name
+    vm.orderByTodo = function() {
+        vm.orderByTodoStartingByA = !vm.orderByTodoStartingByA;
+
+        vm.todos.sort(function(a, b) {
+            var todoA = a.todo.toUpperCase();
+            var todoB = b.todo.toUpperCase();
+
+            if (vm.orderByTodoStartingByA) {
+                if (todoA < todoB) return -1;
+                if (todoA > todoB) return 1;
+            } else {
+                if (todoA < todoB) return 1;
+                if (todoA > todoB) return -1;
+            }
+        });
+    }
+
+    // order todo list by todo priority
+    vm.orderByPriority = function() {
+        vm.orderByPriorityStartingByA = !vm.orderByPriorityStartingByA;
+
+        vm.todos.sort(function(a, b) {
+            var priorityA = a.priority.toUpperCase();
+            var priorityB = b.priority.toUpperCase();
+
+            if (vm.orderByPriorityStartingByA) {
+                if (priorityA < priorityB) return -1;
+                if (priorityA > priorityB) return 1;
+            } else {
+                if (priorityA < priorityB) return 1;
+                if (priorityA > priorityB) return -1;
+            }
+        });
+    }
+
+    // delete todo from list
+    vm.delete = function(index) {
+        vm.todos.splice(index, 1);
+    }
+
+
+    vm.seeArray = function(index){
+      console.log(vm.todos);
+    }
 }
